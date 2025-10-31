904de14ba8fbfb9611833fb6c9dec12503925aa3
wxw-lightning <33735818+wxw-lightning@users.noreply.github.com>
2025-10-31 05:18:18 -0400

    level diff

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -13,23 +13,38 @@ public class Door : MonoBehaviour
         PlayerController player = other.GetComponent<PlayerController>();
         if (player != null)
         {
-            if (player.HasKey())
+            int keysRequired = GetKeysRequired();
+            
+            if (player.GetKeyCount() >= keysRequired)
             {
-                OpenDoor(player);
+                OpenDoor(player, keysRequired);
             }
             else
             {
-                Debug.Log("Need a key to open this door!");
+                Debug.Log("Need " + keysRequired + " keys to open this door! You have: " + player.GetKeyCount());
             }
         }
     }
     
-    void OpenDoor(PlayerController player)
+    int GetKeysRequired()
+    {
+        if (GameManager.Instance != null)
+        {
+            return GameManager.Instance.GetKeysRequired();
+        }
+        return 1;
+    }
+    
+    void OpenDoor(PlayerController player, int keysRequired)
     {
         if (!isOpen)
         {
             isOpen = true;
-            player.UseKey();
+            
+            for (int i = 0; i < keysRequired; i++)
+            {
+                player.UseKey();
+            }
             
             if (GameManager.Instance != null)
             {
@@ -41,7 +56,7 @@ public class Door : MonoBehaviour
                 doorVisual.SetActive(false);
             }
             
-            Debug.Log("Door opened! Score added: " + scoreValue);
+            Debug.Log("Door opened! Used " + keysRequired + " keys. Score added: " + scoreValue);
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,14 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public Text keysText;
     
+    [Header("Difficulty Settings")]
+    public float difficultyIncreaseTime = 30f;
+    public int keysRequiredAfterIncrease = 3;
+    
     private int score = 0;
     private PlayerController player;
+    private float gameTime = 0f;
+    private int currentKeysRequired = 1;
     
     void Awake()
     {
@@ -32,6 +38,14 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
+        gameTime += Time.deltaTime;
+        
+        if (gameTime >= difficultyIncreaseTime && currentKeysRequired == 1)
+        {
+            currentKeysRequired = keysRequiredAfterIncrease;
+            Debug.Log("Difficulty increased! Doors now require " + currentKeysRequired + " keys!");
+        }
+        
         UpdateUI();
     }
     
@@ -57,5 +71,15 @@ public class GameManager : MonoBehaviour
     public int GetScore()
     {
         return score;
+    }
+    
+    public int GetKeysRequired()
+    {
+        return currentKeysRequired;
+    }
+    
+    public float GetGameTime()
+    {
+        return gameTime;
     }
 }
